45776b89c65e2a3ad4260d5cec9f7e950ca78671
Ben Casson <bcasson11@gmail.com>
2022-05-05 15:39:28 -0500

    Update

--- app.js
+++ app.js
@@ -7,37 +7,37 @@ const setTheme = theme => document.documentElement.className = theme;
 
 
 // Change star rating based on user input
-const ratingStars = [...document.getElementsByClassName("rating__star")];
-const ratingResult = document.querySelector(".rating__result");
+
+// const ratingStars = [...document.getElementsByClassName("rating__star")];
+// const ratingResult = document.querySelector(".rating__result");
 
 // printRatingResult(ratingResult);     ///////
 
-function executeRating(stars, result) {
-   const starClassActive = "rating__star fas fa-star";
-   const starClassUnactive = "rating__star far fa-star";
-   const starsLength = stars.length;
-   let i;
-   stars.map((star) => {
-      star.onclick = () => {
-         i = stars.indexOf(star);
+// function executeRating(stars, result) {
+//    const starClassActive = "rating__star fas fa-star";
+//    const starClassUnactive = "rating__star far fa-star";
+//    const starsLength = stars.length;
+//    let i;
+//    stars.map((star) => {
+//       star.onclick = () => {
+//          i = stars.indexOf(star);
 
-         if (star.className.indexOf(starClassUnactive) !== -1) {
-             //printRatingResult(result, i + 1);        ////////
-            for (i; i >= 0; --i) stars[i].className = starClassActive;
-         } 
-         else {
-             //printRatingResult(result, i);        //////
-            for (i; i < starsLength; ++i) stars[i+1].className = starClassUnactive;
-         }
-      };
-   });
-}
+//          if (star.className.indexOf(starClassUnactive) !== -1) {
+//              //printRatingResult(result, i + 1);        ////////
+//             for (i; i >= 0; --i) stars[i].className = starClassActive;
+//          } 
+//          else {
+//              //printRatingResult(result, i);        //////
+//             for (i; i < starsLength; ++i) stars[i+1].className = starClassUnactive;
+//          }
+//       };
+//    });
+// }
+// function printRatingResult(result, num = 0) {
+//    result.textContent = `${num}/5`;
+// }
+// executeRating(ratingStars, ratingResult);
 
-function printRatingResult(result, num = 0) {
-   result.textContent = `${num}/5`;
-}
-
-executeRating(ratingStars, ratingResult);
 // Uncaught TypeError: stars[(i + 1)] is undefined
 // onclick http://127.0.0.1:5500/app.js:26
 // executeRating http://127.0.0.1:5500/app.js:17
