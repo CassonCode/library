be3e22d6b61bd1603cb6f861d0572ca54461ff53
Ben Casson <bcasson11@gmail.com>
2022-05-06 22:27:01 -0500

    Update

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const qs = document.querySelector.bind(document); 
 const qsa = document.querySelectorAll.bind(document);
-// ex: qs('#myButton');
-
+// ex:  qs('#myButton');
 
 
 // Sets class for :root, specifically for switching between light and dark mode
@@ -9,53 +8,11 @@ const setTheme = theme => document.documentElement.className = theme;
 
 
 
-
-
-
-// Change star rating based on user input
-
-// const ratingStars = [...document.getElementsByClassName("rating__star")];
-// const ratingResult = document.querySelector(".rating__result");
-
-// printRatingResult(ratingResult);     ///////
-
-// function executeRating(stars, result) {
-//    const starClassActive = "rating__star fas fa-star";
-//    const starClassUnactive = "rating__star far fa-star";
-//    const starsLength = stars.length;
-//    let i;
-//    stars.map((star) => {
-//       star.onclick = () => {
-//          i = stars.indexOf(star);
-
-//          if (star.className.indexOf(starClassUnactive) !== -1) {
-//              //printRatingResult(result, i + 1);        ////////
-//             for (i; i >= 0; --i) stars[i].className = starClassActive;
-//          } 
-//          else {
-//              //printRatingResult(result, i);        //////
-//             for (i; i < starsLength; ++i) stars[i+1].className = starClassUnactive;
-//          }
-//       };
-//    });
-// }
-// function printRatingResult(result, num = 0) {
-//    result.textContent = `${num}/5`;
-// }
-//executeRating(ratingStars, ratingResult);
-
-// Uncaught TypeError: stars[(i + 1)] is undefined
-// onclick http://127.0.0.1:5500/app.js:26
-// executeRating http://127.0.0.1:5500/app.js:17
-// executeRating http://127.0.0.1:5500/app.js:16
-// <anonymous> http://127.0.0.1:5500/app.js:36
-
-
 //  Change rating on book being added or edited
 const starsArray = [...document.getElementsByClassName("add-book-star-button")]; 
-const starButtons = document.querySelector(".add-book-star-button");
-const starIconFilled = document.querySelector(".add-star-active");
-const starIconOutline = document.querySelector(".add-star-inactive");
+const starButtons = qs(".add-book-star-button");
+const starIconFilled = qs(".add-star-active");
+const starIconOutline = qs(".add-star-inactive");
 
 let newBookRating;
 
@@ -63,11 +20,9 @@ function setStarRating(arr) {
         arr.forEach((starButton) => {       /////// use 'forEach'  or  '.map' ?????
             starButton.onclick = () => {
                 for (let i = 0; i <= arr.indexOf(starButton); i++) {
-                    // console.log(starButton);
                     let currentStarButton = arr[i];
                     let activeStar = currentStarButton.children[1];
                     let inactiveStar = currentStarButton.children[0];
-                    // console.log(currentStar);
                     addStarVisibleClass(activeStar);
                     addStarHiddenClass(inactiveStar);
                     newBookRating = i + 1;
@@ -76,7 +31,6 @@ function setStarRating(arr) {
                     let currentStarButton = arr[i];
                     let activeStar = currentStarButton.children[0];
                     let inactiveStar = currentStarButton.children[1];
-                    // console.log(currentStar);
                     addStarVisibleClass(activeStar);
                     addStarHiddenClass(inactiveStar);
                 }
@@ -84,7 +38,6 @@ function setStarRating(arr) {
             }
         });
 }
-// setStarRating(starsArray, currentStatusText);
 
 function addStarVisibleClass(star) {
     star.classList.remove("star-hidden");
@@ -96,7 +49,6 @@ function addStarHiddenClass(star) {
 }
 
 starButtons.addEventListener("click", setStarRating(starsArray));
-
 
 
 const statusDropdown = document.getElementById("status-dropdown");
@@ -136,16 +88,13 @@ window.onload = () => statusDropdown.value.selected = "finished";
 
 
 
-
-
 //  Adds/Removes css classes that open/close menu
-const menuButton = document.querySelector(".menu-button");
-const menuIcon = document.querySelector(".menu-icon");
-const mobileSidebar = document.querySelector(".sidebar-menu-container");
+const menuButton = qs(".menu-button");
+const menuIcon = qs(".menu-icon");
+const mobileSidebar = qs(".sidebar-menu-container");
 
 menuButton.addEventListener("click", () => {
     (menuIcon.classList.contains("open-menu")) ? openMenu() : closeMenu();
-    // (mobileSidebar.classList.contains("show-menu")) ? closeMenu() : openMenu();
     console.log(menuIcon.classList);
 });
 
@@ -167,8 +116,8 @@ function closeMenu() {
 
 
 //  Rotating sort normal/reverse icon on click
-const sortNormalReverseButton = document.querySelector(".sort-normal-reverse-button");
-const sortNormalReverseIcon = document.querySelector(".sort-normal-reverse-icon");
+const sortNormalReverseButton = qs(".sort-normal-reverse-button");
+const sortNormalReverseIcon = qs(".sort-normal-reverse-icon");
 
 sortNormalReverseButton.addEventListener("click", () => {
     (sortNormalReverseIcon.classList.contains("sort-normal")) ? setSortingIconToReverseOrder() : setSortingIconToNormalOrder();
